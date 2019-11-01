40872900f0597ffd6a84573284a245ddfcd4b0ec
vu phan <vuphan@vus-macbook-pro.attlocal.net>
2019-11-01 00:40:16 -0700

    update search template and test new clock.py

--- clock.py
+++ clock.py
@@ -35,34 +35,34 @@ def send_email():
     smtp_server = "smtp.gmail.com"
     sender_email = "weat4747@gmail.com"
     password = os.getenv("PASS_EMAIL")
-    # if len(berkeley_list) != 0:
-    #     receiver_email = ", ".join(berkeley_list)
-    #     message = MIMEMultipart("alternative")
-    #     message["Subject"] = "Hello from We@t"
-    #     message["From"] = sender_email
-    #     message["To"] = receiver_email
-    #     text = """\
-    #         Good morning!"""
-    #     html = """\
-    #         <html>
-    #             <body>
-    #                 <p>This is your linked to all recommended restaurant<br>
-    #                 <a href="https://weat-app.herokuapp.com/search?q=Berkeley"><strong>Click here</strong></a>
-    #                 </p>
-    #             </body>
-    #         </html>
-    #         """
-    #
-    #     part1 = MIMEText(text, "plain")
-    #     part2 = MIMEText(html, "html")
-    #
-    #     message.attach(part1)
-    #     message.attach(part2)
-    #
-    #     context = ssl.create_default_context()
-    #     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    #         server.login(sender_email, password)
-    #         server.sendmail(sender_email, receiver_email, message.as_string())
+    if len(berkeley_list) != 0:
+        receiver_email = ", ".join(berkeley_list)
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Hello from We@t"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        text = """\
+            Good morning!"""
+        html = """\
+            <html>
+                <body>
+                    <p>This is your linked to all recommended restaurant<br>
+                    <a href="https://weat-app.herokuapp.com/search?q=Berkeley"><strong>Click here</strong></a>
+                    </p>
+                </body>
+            </html>
+            """
+
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+        message.attach(part1)
+        message.attach(part2)
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
 
     if len(oak_list) != 0:
         receiver_email = ", ".join(oak_list)
@@ -93,32 +93,32 @@ def send_email():
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
 
-    # if len(sf_list) != 0:
-    #     receiver_email = ", ".join(sf_list)
-    #     message = MIMEMultipart("alternative")
-    #     message["Subject"] = "Hello from We@t"
-    #     message["From"] = sender_email
-    #     message["To"] = receiver_email
-    #     text = """\
-    #         Good morning!"""
-    #     html = """\
-    #         <html>
-    #             <body>
-    #                 <p>This is your linked to all recommended restaurant<br>
-    #                 <a href="https://weat-app.herokuapp.com/search?q=San+Francisco"><strong>Click here</strong></a>
-    #                 </p>
-    #             </body>
-    #         </html>
-    #         """
-    #
-    #     part1 = MIMEText(text, "plain")
-    #     part2 = MIMEText(html, "html")
-    #
-    #     message.attach(part1)
-    #     message.attach(part2)
-    #
-    #     context = ssl.create_default_context()
-    #     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    #         server.login(sender_email, password)
-    #         server.sendmail(sender_email, receiver_email, message.as_string())
+    if len(sf_list) != 0:
+        receiver_email = ", ".join(sf_list)
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Hello from We@t"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        text = """\
+            Good morning!"""
+        html = """\
+            <html>
+                <body>
+                    <p>This is your linked to all recommended restaurant<br>
+                    <a href="https://weat-app.herokuapp.com/search?q=San+Francisco"><strong>Click here</strong></a>
+                    </p>
+                </body>
+            </html>
+            """
+
+        part1 = MIMEText(text, "plain")
+        part2 = MIMEText(html, "html")
+
+        message.attach(part1)
+        message.attach(part2)
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
 sched.start()
